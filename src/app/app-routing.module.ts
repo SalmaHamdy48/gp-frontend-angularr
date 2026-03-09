55631d1d22219baa56f1f92b0b88e6a44b69e862
Salma <salma.hamdy@appnest.pro>
2026-03-09 17:46:13 +0200

    feat:Added Virtual Try-On page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { VirtualTryOnComponent } from './pages/virtual-try-on/virtual-try-on.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'vto', component: VirtualTryOnComponent }
+  // { path: 'recommendations', component: RecommendationsComponent },
+  // { path: 'profile', component: ProfileComponent },
+  //{ path: '', redirectTo: 'vto', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { VirtualTryOnComponent } from './pages/virtual-try-on/virtual-try-on.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    VirtualTryOnComponent
   ],
   imports: [
     BrowserModule,
