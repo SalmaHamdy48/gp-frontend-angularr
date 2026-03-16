a0e4d3340ebaf4ee6adf7c16472696aca8d6b921
Amira <amiramostafa942004@gmail.com>
2026-03-16 21:32:01 +0200

    Added profile page

--- src/app/features/auth/pages/recommendation/recommendation.component.ts
+++ src/app/features/auth/pages/recommendation/recommendation.component.ts
@@ -9,9 +9,6 @@
 
 // }
 
-
-
-
 import { Component, OnInit } from '@angular/core';
 
 interface Occasion {
@@ -22,12 +19,12 @@ interface Occasion {
 @Component({
   selector: 'app-recommendation',
   templateUrl: './recommendation.component.html',
-  styleUrls: ['./recommendation.component.scss']
+  styleUrls: ['./recommendation.component.scss'],
 })
 export class RecommendationComponent implements OnInit {
-deleteItem(_t45: any,arg1: string) {
-throw new Error('Method not implemented.');
-}
+  deleteItem(_t45: any, arg1: string) {
+    throw new Error('Method not implemented.');
+  }
 
   isClosetEmpty: boolean = true;
   selectedTab: string = 'use-closet';
@@ -42,30 +39,21 @@ throw new Error('Method not implemented.');
     { id: 'date', label: 'date night' },
     { id: 'workout', label: 'Workout/Athletic' },
     { id: 'beach', label: 'Beach/vacation' },
-    { id: 'wedding', label: 'wedding' }
+    { id: 'wedding', label: 'wedding' },
   ];
 
-
   closet = {
-  tops: [],
-  bottoms: [],
-  shoes: []
-};
+    tops: [],
+    bottoms: [],
+    shoes: [],
+  };
 
-
-currentCategory: string = '';
-
-tops: any[] = [];
-bottoms: any[] = [];
-shoes: any[] = [];
-
-
-
-
-currentType: string = '';
-
+  currentCategory: string = '';
+  tops: any[] = [];
+  bottoms: any[] = [];
+  shoes: any[] = [];
+  currentType: string = '';
   activeTab: string = 'closet';
-
   // Modal
   showAddItemModal: boolean = false;
 
@@ -75,278 +63,100 @@ currentType: string = '';
     color: '',
     gender: '',
     season: '',
-    occasion: ''
+    occasion: '',
   };
 
   closetItems: any[] = [
     // Mock data - would be populated from profile
   ];
 
-selectedFile!: File;
+  selectedFile!: File;
+  selectedCategory: string = '';
+  previewImage: string = '';
+  selectedImage: string | null = null;
+  editingItem: any = null;
+  editingCategory: string = '';
 
-selectedCategory: string = '';
-previewImage: string = '';
-selectedImage: string | null = null;
-
-
-editingItem: any = null;
-editingCategory: string = '';
-
-// this.selectedCategory = type;
+  // this.selectedCategory = type;
   constructor() {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.fetchCloset();
   }
 
-
-openAddItem(category: string){
+  openAddItem(category: string) {
     this.selectedCategory = category;
     this.showAddItemModal = true;
   }
 
- 
+  addItem() {
+    if (this.editingItem) {
+      this.editingItem.name = this.newItem.name;
+      this.editingItem.type = this.newItem.type;
+      this.editingItem.color = this.newItem.color;
+      this.editingItem.gender = this.newItem.gender;
+      this.editingItem.season = this.newItem.season;
+      this.editingItem.occasion = this.newItem.occasion;
+    } else {
+      const item = {
+        image: this.selectedImage,
+        ...this.newItem,
+      };
 
-  // addItem(){
-  //   console.log('Item added:', this.newItem);
+      if (this.currentCategory === 'top') this.tops.push(item);
+      if (this.currentCategory === 'bottom') this.bottoms.push(item);
+      if (this.currentCategory === 'shoe') this.shoes.push(item);
+    }
 
-  //   this.showAddItemModal = false;
-  // }
-
-//   addItem(){
-
-//   const item = {
-//     name: this.newItem.name,
-//     type: this.newItem.type,
-//     color: this.newItem.color,
-//     gender: this.newItem.gender,
-//     season: this.newItem.season,
-//     image: this.previewImage
-//   };
-
-//   if(this.selectedCategory === 'top'){
-//     this.tops.push(item);
-//   }
-
-//   if(this.selectedCategory === 'bottom'){
-//     this.bottoms.push(item);
-//   }
-
-//   if(this.selectedCategory === 'shoe'){
-//     this.shoes.push(item);
-//   }
-
-//   this.showAddItemModal = false;
-
-// }
-
-
-
-
-// addItem() {
-
-//   const item = {
-//     name: this.newItem.name,
-//     type: this.newItem.type,
-//     color: this.newItem.color,
-//     gender: this.newItem.gender,
-//     season: this.newItem.season,
-//     // image: this.previewImage
-//     image: this.selectedImage
-//   };
-
-//   if (this.selectedCategory === 'top') {
-//     this.tops.push(item);
-//   }
-
-//   if (this.selectedCategory === 'bottom') {
-//     this.bottoms.push(item);
-//   }
-
-//   if (this.selectedCategory === 'shoe') {
-//     this.shoes.push(item);
-//   }
-
-//   // this.showAddItemModal = false;
-//   this.closeAddItem();
-// }
-
-
-
-addItem() {
-
-if (this.editingItem) {
-
-this.editingItem.name = this.newItem.name;
-this.editingItem.type = this.newItem.type;
-this.editingItem.color = this.newItem.color;
-this.editingItem.gender = this.newItem.gender;
-this.editingItem.season = this.newItem.season;
-this.editingItem.occasion = this.newItem.occasion;
-
-} else {
-
-const item = {
-image: this.selectedImage,
-...this.newItem
-};
-
-if (this.currentCategory === 'top') this.tops.push(item);
-if (this.currentCategory === 'bottom') this.bottoms.push(item);
-if (this.currentCategory === 'shoe') this.shoes.push(item);
-
-}
-
-this.closeAddItem();
-
-}
-fetchCloset(){
-  // call backend service
-}
+    this.closeAddItem();
+  }
+  fetchCloset() {
+    // call backend service
+  }
 
   switchTab(tab: string): void {
     this.selectedTab = tab;
   }
 
-
-  setTab(tab: string){
+  setTab(tab: string) {
     this.activeTab = tab;
   }
   selectOccasion(occasion: string): void {
     this.selectedOccasion = occasion;
   }
 
-//   onImageSelected(event: any, category: string) {
+  onImageSelected(event: any, category: string) {
+    const files: FileList = event.target.files;
+    if (!files) return;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.onload = () => {
+        const item = {
+          name: file.name.split('.').slice(0, -1).join('.'),
+          image: reader.result as string,
+          type: '',
+          color: '',
+          gender: '',
+          season: '',
+        };
 
-//   const file = event.target.files[0];
+        if (category === 'top') {
+          this.tops.push(item);
+        }
 
-//   if (!file) return;
+        if (category === 'bottom') {
+          this.bottoms.push(item);
+        }
 
-//   this.selectedFile = file;
-//   this.selectedCategory = category;
+        if (category === 'shoe') {
+          this.shoes.push(item);
+        }
+      };
 
-//   const reader = new FileReader();
-
-//   reader.onload = () => {
-//     this.previewImage = reader.result;
-//     this.showAddItemModal = true; // OPEN MODAL AFTER IMAGE
-//   };
-
-//   reader.readAsDataURL(file);
-// }
-
-// onImageSelected(event: any, category: string) {
-
-//   const file = event.target.files[0];
-//   if (!file) return;
-
-//   this.selectedCategory = category;
-
-//   // ✅ Auto-fill item name with file name (without extension)
-//   const fileName = file.name.split('.').slice(0, -1).join('.');
-//   this.newItem.name = fileName;
-
-//   const reader = new FileReader();
-
-//   reader.onload = () => {
-//     this.previewImage = reader.result;
-//     this.showAddItemModal = true;
-//   };
-
-//   reader.readAsDataURL(file);
-// }
-
-
-// onImageSelected(event: any, category: string) {
-
-//   const file = event.target.files[0];
-//   if (!file) return;
-
-//   this.selectedCategory = category;
-
-//   const reader = new FileReader();
-
-//   reader.onload = () => {
-
-//     const result = reader.result;
-
-//     if (typeof result === 'string') {
-//       this.previewImage = result;
-//     }
-
-//     this.newItem.name = file.name.split('.')[0];
-
-//     this.showAddItemModal = true;
-//   };
-
-//   reader.readAsDataURL(file);
-// }
-
-// onImageSelected(event: any, type: string) {
-
-//   const file = event.target.files[0];
-
-//   if (file) {
-
-//     // ✅ Automatically set item name from device file name
-//     this.newItem.name = file.name;
-//     // without Extension
-//     this.newItem.name = file.name.split('.').slice(0, -1).join('.');
-
-//     const reader = new FileReader();
-
-//     reader.onload = () => {
-//       this.selectedImage = reader.result as string;
-//       this.showAddItemModal = true;
-//     };
-
-//     reader.readAsDataURL(file);
-//   }
-// }
-
-onImageSelected(event: any, category: string) {
-
-const files: FileList = event.target.files;
-
-if (!files) return;
-
-for (let i = 0; i < files.length; i++) {
-
-const file = files[i];
-
-const reader = new FileReader();
-
-reader.onload = () => {
-
-const item = {
-name: file.name.split('.').slice(0, -1).join('.'),
-image: reader.result as string,
-type: '',
-color: '',
-gender: '',
-season: ''
-};
-
-if (category === 'top') {
-this.tops.push(item);
-}
-
-if (category === 'bottom') {
-this.bottoms.push(item);
-}
-
-if (category === 'shoe') {
-this.shoes.push(item);
-}
-
-};
-
-reader.readAsDataURL(file);
-
-}
-
-}
-
+      reader.readAsDataURL(file);
+    }
+  }
 
   getRecommendations(): void {
     if (this.selectedOccasion) {
@@ -356,44 +166,29 @@ reader.readAsDataURL(file);
     }
   }
 
-  
-
   goToProfile(): void {
     // Navigate to profile to add items
     console.log('Navigate to profile');
   }
-
-
-
   editItem(item: any, category: string) {
+    this.editingItem = item;
+    this.editingCategory = category;
+    this.newItem = { ...item }; // copy values into form
+    this.selectedImage = item.image;
+    this.showAddItemModal = true;
+  }
 
-this.editingItem = item;
-this.editingCategory = category;
+  closeAddItem() {
+    this.showAddItemModal = false;
+    this.editingItem = null;
 
-this.newItem = { ...item };   // copy values into form
-this.selectedImage = item.image;
-
-this.showAddItemModal = true;
-
+    this.newItem = {
+      name: '',
+      type: '',
+      color: '',
+      gender: '',
+      season: '',
+      occasion: '',
+    };
+  }
 }
-
-
-
-closeAddItem(){
-
-this.showAddItemModal = false;
-this.editingItem = null;
-
-this.newItem = {
-name:'',
-type:'',
-color:'',
-gender:'',
-season:'',
-occasion:''
-};
-
-}
-}
-
-
--- src/app/features/auth/pages/signup/signup.component.ts
+++ src/app/features/auth/pages/signup/signup.component.ts
@@ -12,87 +12,51 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-
-
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common'; // for *ngIf etc.
-
-
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
-// export class SignupComponent {
-// onSignUp() {
-// throw new Error('Method not implemented.');
-// }
-// onLogIn: any;
-
-//   Name: string = '';
-//   Email: string = '';
-//   Password: string = '';
-// signupObj: any;
-// loginObj: any;
-
-//   constructor(
-//     private authService: AuthService,
-//     private router: Router
-//   ) {}
-
-//   onSubmit() {
-//     this.authService.signup(this.Name, this.Email, this.Password);
-//     console.log('Signup submitted');
-//   }
-
-//   goToLogin() {
-//     this.router.navigate(['/auth/login']);
-//   }
-// }
-
-
-
 export class SignupComponent implements OnInit {
-signupData = {
+  signupData = {
     name: '',
     email: '',
-    password: ''
+    password: '',
   };
-// onSignUp() {
-// throw new Error('Method not implemented.');
-// }
-// onLogIn() {
-// throw new Error('Method not implemented.');
-// }
+
   signupForm!: FormGroup;
   submitted = false;
   router: any;
 
-  // constructor(private formBuilder: FormBuilder) {}
-
-  
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.signupForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   // Convenience getter for easy access to form fields
-  get f() { return this.signupForm.controls; }
+  get f() {
+    return this.signupForm.controls;
+  }
 
-  
   onSubmit() {
-    // console.log('Form submitted:', this.signupData);
-    // Here you would call your authentication service
-
-
-     if (this.signupData.name && this.signupData.email && this.signupData.password) {
+    if (
+      this.signupData.name &&
+      this.signupData.email &&
+      this.signupData.password
+    ) {
       // You could also add more validation or an actual HTTP request here.
       console.log('Signup successful', this.signupData);
 
@@ -103,4 +67,3 @@ signupData = {
     }
   }
 }
-
